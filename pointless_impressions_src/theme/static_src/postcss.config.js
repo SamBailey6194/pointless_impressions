dbf1f90ba80e62ccc22b0e2e58d88302a5481d38
SamBailey6194 <samabailey6194@gmail.com>
2025-10-24 12:51:13 +0100

    build(staging): Move package.json and package-lock.json to root
    Heroku's nodejs buildpack couldn't fine the package.json files. Moved to root to find them easily and updated the scripts.
    
    Added a hash-css.js to handle hashing the css for build.
    
    Files Changed:
    - .sligignore
    - package.json
    - package-lock.json
    - pointless_impressions_src/theme/static_src/postcss.config.js
    - pointless_impressions_src/theme/static_src/src/build.js/hash-css.js
    
    Still to do:
    - Deploy staging to Heroku
    - Update relevant markdoiwns with the changes for running npm commands

--- pointless_impressions_src/theme/static_src/postcss.config.js
+++ pointless_impressions_src/theme/static_src/postcss.config.js
@@ -1,18 +1,9 @@
 import tailwindcss from '@tailwindcss/postcss';
 import autoprefixer from 'autoprefixer';
-import postcssHash from 'postcss-hash';
-
-const production = process.env.NODE_ENV === 'production';
 
 export default {
   plugins: [
     tailwindcss,
     autoprefixer,
-    production &&
-      postcssHash({
-        manifest: '../../static/css/manifest.json',
-        algorithm: 'md5',
-        trim: 8,
-      }),
-  ].filter(Boolean),
+  ],
 };
