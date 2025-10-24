2c15a91e15ade0c6070c0e86a2506e8d22843135
SamBailey6194 <samabailey6194@gmail.com>
2025-10-24 13:19:42 +0100

    build(staging): Add verbose naming for apps in app.py

--- pointless_impressions_src/home/apps.py
+++ pointless_impressions_src/home/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'home'
+    name = 'pointless_impressions_src.home'
