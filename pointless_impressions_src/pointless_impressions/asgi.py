925529fdded1a53ef13416e08900c43f7b03e161
SamBailey6194 <samabailey6194@gmail.com>
2025-10-24 13:56:37 +0100

    build(staging): More verbose about where setting files are

--- pointless_impressions_src/pointless_impressions/asgi.py
+++ pointless_impressions_src/pointless_impressions/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pointless_impressions.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pointless_impressions_src.pointless_impressions.settings')
 
 application = get_asgi_application()
