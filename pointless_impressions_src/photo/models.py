8d56aa3761fbc0e26aef7fcb8d560cd0a1c63e61
SamBailey6194 <samabailey6194@gmail.com>
2025-11-15 21:15:00 +0000

    Removed unnecessary conditional logic for the image handling.

--- pointless_impressions_src/photo/models.py
+++ pointless_impressions_src/photo/models.py
@@ -4,16 +4,6 @@ from django.conf import settings
 from cloudinary.models import CloudinaryField
 
 
-# Conditionally use CloudinaryField for staging/production,
-# ImageField for local
-if settings.DEBUG:
-    # Local development: use standard ImageField
-    PhotoField = models.ImageField
-else:
-    # Staging/Production: use CloudinaryField
-    PhotoField = CloudinaryField
-
-
 # Create your models here.
 def artwork_image_path(instance, filename):
     """Determine upload path based on associated model."""
@@ -77,8 +67,7 @@ class Photo(models.Model):
     image = CloudinaryField(
         'image',
         blank=False,
-        null=False,
-        upload_to=""
+        null=False
     )
 
     alt_text = models.CharField(max_length=255, blank=True)
